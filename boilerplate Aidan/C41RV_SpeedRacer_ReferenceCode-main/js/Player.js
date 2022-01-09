0e56ec5f44f7b0c0393dbcbef6a43993097b20db
pkhurana-whjr <73402498+pkhurana-whjr@users.noreply.github.com>
2022-01-10 01:31:29 +0530

    Add files via upload

---
+++ boilerplate Aidan/C41RV_SpeedRacer_ReferenceCode-main/js/Player.js
@@ -0,0 +1,79 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+    //Step 1: C41
+    this.rank = 0;
+    this.fuel = 185;
+    this.life = 185;
+    this.score = 0;
+    //step 2: add CarsAtEnd in the DB
+  }
+
+  addPlayer() {
+    var playerIndex = "players/player" + this.index;
+
+    if (this.index === 1) {
+      this.positionX = width / 2 - 100;
+    } else {
+      this.positionX = width / 2 + 100;
+    }
+
+    database.ref(playerIndex).set({
+      name: this.name,
+      positionX: this.positionX,
+      positionY: this.positionY,
+      rank: this.rank,
+      score: this.score
+    });
+  }
+
+  getDistance() {
+    var playerDistanceRef = database.ref("players/player" + this.index);
+    playerDistanceRef.on("value", data => {
+      var data = data.val();
+      this.positionX = data.positionX;
+      this.positionY = data.positionY;
+    });
+  }
+
+  getCount() {
+    var playerCountRef = database.ref("playerCount");
+    playerCountRef.on("value", data => {
+      playerCount = data.val();
+    });
+  }
+
+  updateCount(count) {
+    database.ref("/").update({
+      playerCount: count
+    });
+  }
+
+  update() {
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).update({
+      positionX: this.positionX,
+      positionY: this.positionY,
+      rank: this.rank,
+      score: this.score
+    });
+  }
+
+  static getPlayersInfo() {
+    var playerInfoRef = database.ref("players");
+    playerInfoRef.on("value", data => {
+      allPlayers = data.val();
+    });
+  }
+//Step 3: Create read and write function for rank
+  getCarsAtEnd() {
+   
+  }
+
+  static updateCarsAtEnd(rank) {
+    
+  }
+}
